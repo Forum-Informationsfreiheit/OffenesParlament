b9c6e8eecb01e2711e2d5850df5ca207c64f3825
Florian Cech <loki@fsinf.at>
2015-03-05 17:33:06 +0100

    Basic Django models added
    Will need additional tweaking, particularly:
    
      * Check null/blank/mandatory field settings
      * Check field defaults

--- offenesparlament/op_scraper/admin.py
+++ offenesparlament/op_scraper/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from op_scraper.models import Phase
+from op_scraper.models import Entity
+from op_scraper.models import Document
+from op_scraper.models import PressRelease
+from op_scraper.models import Category
+from op_scraper.models import Keyword
+from op_scraper.models import Law
+from op_scraper.models import Step
+from op_scraper.models import Opinion
 
-# Register your models here.
+
+admin.site.register(Phase)
+admin.site.register(Entity)
+admin.site.register(Document)
+admin.site.register(PressRelease)
+admin.site.register(Category)
+admin.site.register(Keyword)
+admin.site.register(Law)
+admin.site.register(Step)
+admin.site.register(Opinion)
--- offenesparlament/op_scraper/models.py
+++ offenesparlament/op_scraper/models.py
@@ -1,9 +1,120 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Phase(models.Model):
+
+    """
+    A phase in the process of a law, grouping steps together
+    """
+    title = models.CharField(max_length=255)
+
+
+class Entity(models.Model):
+
+    """
+    An organisation or person commenting in a pre-parliamentary process (prelaw)
+    """
+    title = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = PhoneNumberField()
+
+
+class Document(models.Model):
+
+    """
+    A linked document
+
+    Optionally with stripped html content of html-version
+    """
+    title = models.CharField(max_length=255)
+    pdf_link = models.URLField(max_length=200, null=True, blank=True)
+    html_link = models.URLField(max_length=200, null=True, blank=True)
+    stripped_html = models.TextField(null=True, blank=True)
+
+
+class PressRelease(models.Model):
+
+    """
+    A press release produced by the parliamentary staff
+    """
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    full_text = models.TextField()
+    release_date = models.DateField()
+    source_link = models.URLField(max_length=200, default="")
+    parl_id = models.CharField(max_length=30, unique=True, default="")
+    topics = models.CharField(max_length=255)
+    format = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+
+    """
+    A category for a law or prelaw
+    """
+    title = models.CharField(max_length=255)
+
+
+class Keyword(models.Model):
+
+    """
+    A keyword assigned to laws and prelaws
+    """
+    title = models.CharField(max_length=255)
 
 
 class Law(models.Model):
-    title = models.CharField(max_length=200)
-    # id as given by the parliament's webpage
-    p_id = models.CharField(max_length=50)
+
+    """
+    A single 'Verhandlungssache' or negotiable matter
+
+    Can also be a prelaw (Vorparlamentarischer Prozess,
+        i.e. Ministerialentwurf)
+
+    """
+    title = models.CharField(max_length=255)
+    status = models.TextField(null=True, blank=True)
+    source_link = models.URLField(max_length=200, default="")
+    parl_id = models.CharField(max_length=30, unique=True, default="")
+    description = models.TextField(blank=True)
+
+    # Relationships
+    category = models.ForeignKey(Category, null=True, blank=True)
+    keywords = models.ManyToManyField(Keyword)
+    press_releases = models.ManyToManyField(PressRelease)
+    documents = models.ManyToManyField(Document)
+    references = models.OneToOneField("self", blank=True, null=True)
+
+
+class Step(models.Model):
+
+    """
+    A single step in the parliamentary process
+    """
+    title = models.CharField(max_length=255)
+    sortkey = models.IntegerField()
+    date = models.DateField()
+
+    # Relationships
+    phase = models.ForeignKey(Phase)
+    law = models.ForeignKey(Law)
+
+
+class Opinion(models.Model):
+    """
+    A comment in the pre-parliamentary process by an entity
+    """
+    parl_id = models.CharField(max_length=30, unique=True, default="")
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    # Relationships
+    documents = models.ManyToManyField(Document)
+    category = models.ForeignKey(Category, null=True, blank=True)
+    keywords = models.ManyToManyField(Keyword)
+    entity = models.ForeignKey(Entity)
+    steps = models.ManyToManyField(Step)
+    prelaw = models.ForeignKey(Law)
