2e6d7cd93ff7a4712220e09b3c24ca5215358e06
Florian Cech <loki@fsinf.at>
2015-06-04 16:09:02 +0200

    Added preliminary detail-views for laws

--- offenesparlament/offenesparlament/urls.py
+++ offenesparlament/offenesparlament/urls.py
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='home'),
     url(r'^parlamentarier/$', views.person_list, name='person_list'),
     url(r'^parlamentarier/(?P<parl_id>.{1,30})/(?P<name>.+)/$', views.person_detail, name='person_detail'),
+    url(r'^gesetze/$', views.gesetze_list, name='laws_list'),
+    url(r'^gesetze/(?P<ggp>.{1,30})/(?P<parl_id>.{1,30})/$', views.gesetz_detail, name='gesetz_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- offenesparlament/offenesparlament/views.py
+++ offenesparlament/offenesparlament/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from op_scraper.models import Person
+from op_scraper.models import Law
 
 
 def index(request):
     return render(request, 'base.html')
+
 
 def person_list(request):
     person_list = Person.objects.order_by('reversed_name')
@@ -16,7 +18,27 @@ def person_list(request):
     context = {'person_list': person_list}
     return render(request, 'person_list.html', context)
 
+
+def gesetze_list(request):
+    gesetze_list = Law.objects.order_by('parl_id')
+    # lists = []
+    # sublist_length = ceil(len(person_list) / 3.0)
+    # for i in range(3):
+    #     start = int(i*sublist_length)
+    #     end = int(min(len(person_list), (i+1)*sublist_length))
+    #     lists.append(person_list[start:end])
+    context = {'gesetze_list': gesetze_list}
+    return render(request, 'gesetze_list.html', context)
+
+
 def person_detail(request, parl_id, name):
-    person = Person.objects.get(pk=parl_id)
+    person = Person.objects.get(parl_id=parl_id)
     context = {'person': person}
     return render(request, 'person_detail.html', context)
+
+
+def gesetz_detail(request, parl_id, ggp):
+    parl_id_restored = '({})'.format(parl_id.replace('.', '/'))
+    gesetz = Law.objects.get(parl_id=parl_id_restored, legislative_period=ggp)
+    context = {'gesetz': gesetz}
+    return render(request, 'gesetz_detail.html', context)
