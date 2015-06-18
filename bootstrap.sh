f33ee3aad54ca1e938315c84d10c10ec4bf3fa98
Florian Cech <loki@fsinf.at>
2015-06-18 12:05:40 +0200

    Added elasticsearch installation to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,35 @@
 #!/bin/bash -l
 
 #BASE
-sudo apt-get update
-sudo apt-get install -y python python-pip python-twisted vim curl
+sudo apt-get -qq update
+sudo apt-get install -y python python-pip python-twisted vim curl python-software-properties
 
 # requirements for scrapy
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install -y python-dev libxml2-dev libxslt-dev libffi-dev
 
 # requirements for django extensions
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install -y graphviz
+
+# elasticsearch & requirements
+# open JDK 7
+# kopf plugin for elastic search
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+add-apt-repository -r "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main"
+echo "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get -qq update
+sudo apt-get -y install openjdk-7-jre elasticsearch
+sudo sed -i 's/\/var\/run\/elasticsearch/\/var\/run/g' /etc/init.d/elasticsearch
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /usr/share/elasticsearch/bin/plugin --install lmenezes/elasticsearch-kopf
+sudo /etc/init.d/elasticsearch restart
+
 
 # install node.js and NPM
 # install PPA first to get recent package
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install -y nodejs build-essential
 
 # install client project requirements and the grunt-CLI task runner globally
