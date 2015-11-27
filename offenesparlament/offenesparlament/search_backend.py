442967534704b5eef50cd551658ef5f03db27f6c
Florian Cech <loki@fsinf.at>
2015-11-27 16:58:53 +0100

    Fixes bug in search backend when not handing over query string

--- offenesparlament/offenesparlament/search_backend.py
+++ offenesparlament/offenesparlament/search_backend.py
@@ -2,24 +2,36 @@ from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend
 from haystack.backends.elasticsearch_backend import ElasticsearchSearchQuery
 from haystack.backends import BaseEngine
 
+
 class FuzzyElasticsearchSearchBackend(ElasticsearchSearchBackend):
+
     """
     Custom ES Backend that fuzzyfies the Haystack default query_string queries
     """
+
     def build_search_kwargs(self, query_string, **kwargs):
         fuzzy = kwargs.pop('fuzzy', True)
         fuzzy_field = kwargs.pop('min_similarity', '')
         search_kwargs = super(FuzzyElasticsearchSearchBackend, self).build_search_kwargs(
-                query_string, **kwargs)
-        if fuzzy:
-            query_string = search_kwargs['query']['filtered']['query']['query_string']['query']
-            query_string = u'({}~)'.format(
-                query_string[1:-1].replace(' ', '~ '))
-            search_kwargs['query']['filtered']['query']['query_string']['query'] = query_string
-
+            query_string, **kwargs)
+        if fuzzy and 'query_string' in search_kwargs['query']['filtered']['query']:
+            try:
+                # fuzzyfy query strings
+                query_string = search_kwargs['query'][
+                    'filtered']['query']['query_string']['query']
+                query_string = u'({}~)'.format(
+                    query_string[1:-1].replace(' ', '~ '))
+                search_kwargs['query']['filtered']['query'][
+                    'query_string']['query'] = query_string
+            except:
+                # something went wrong, maybe we don't have a query_string
+                # query after all?
+                pass
         return search_kwargs
 
+
 class FuzzyElasticsearchSearchEngine(BaseEngine):
+
     """
     Custom ES Engine that allows fuzzy search
     """
