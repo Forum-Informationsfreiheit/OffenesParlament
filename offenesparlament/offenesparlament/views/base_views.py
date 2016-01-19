be62b03c69a607d262b266024c77c2806754d753
Ben Freundorfer <benjamin@freundorfer.at>
2016-01-19 10:36:45 +0100

    ordered person list by reversed name

--- offenesparlament/offenesparlament/views/base_views.py
+++ offenesparlament/offenesparlament/views/base_views.py
@@ -40,6 +40,7 @@ def person_list_with_ggp(request, ggp):
     llp = _ensure_ggp_is_set(request, ggp)
     person_list = Person.objects \
             .filter(mandates__legislative_period=llp) \
+            .order_by('reversed_name') \
             .select_related('latest_mandate__party')
     context = {'person_list': person_list}
     return render(request, 'person_list.html', context)
