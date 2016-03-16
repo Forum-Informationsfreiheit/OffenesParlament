f1a66bc062f3ae1ea247bd628fdc1c018fc19c7a
Ben Freundorfer <benjamin@freundorfer.at>
2016-03-16 14:21:32 +0100

    styled "vorparlamentarischer Prozess" and make law subscribable

--- offenesparlament/offenesparlament/views/base_views.py
+++ offenesparlament/offenesparlament/views/base_views.py
@@ -10,6 +10,7 @@ from offenesparlament.views.search import PersonSearchView
 from op_scraper.search_indexes import extract_json_fields
 
 import datetime
+import urllib
 
 
 def index(request):
@@ -130,7 +131,14 @@ def gesetz_detail(request, parl_id, ggp=None):
     else:
         llp = None
         gesetz = Law.objects.get(parl_id=parl_id, legislative_period=llp)
-    context = {'law': gesetz}
+    subscription_title = "{} ({})".format(gesetz.title, gesetz.legislative_period.roman_numeral)
+    url_params = {'parl_id': gesetz.parl_id, 'llps': gesetz.legislative_period.roman_numeral}
+    subscription_url = '/suche/gesetze?{}'.format(urllib.urlencode(url_params))
+    context = {
+        'law': gesetz,
+        'subscription_url': subscription_url,
+        'subscription_title': subscription_title
+    }
     return render(request, 'gesetz_detail.html', context)
 
 
