3043df596ef721b4bc7039113a69ae0fd1f760d0
Florian Cech <lyrixderaven@gmail.com>
2016-03-31 13:30:24 +0200

    Add daily and weekly run scripts for production

---
+++ offenesparlament/bin/run_weekly.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FOLDER="/vagrant/offenesparlament"
+if [ "$#" -gt 0 ]; then
+  FOLDER=$1
+fi
+
+CMD="python manage.py"
+if [ "$#" -gt 1 ]; then
+  CMD=$2
+fi
+
+
+cd $FOLDER
+
+eval "$CMD scrape crawl llp"
+eval "$CMD scrape crawl persons"
+eval "$CMD scrape crawl administrations"
+eval "$CMD scrape crawl pre_laws"
+eval "$CMD scrape crawl laws_initiatives"
+eval "$CMD scrape crawl auditors"
+eval "$CMD scrape crawl comittees"
+eval "$CMD scrape crawl inquiries"
+eval "$CMD scrape crawl petitions"
+eval "$CMD scrape crawl statement"
+
+# run es update
+eval "$CMD update_index op_scraper.Debate -b 100"
+eval "$CMD update_index op_scraper.Law -b 1000"
+eval "$CMD update_index op_scraper.Person"
