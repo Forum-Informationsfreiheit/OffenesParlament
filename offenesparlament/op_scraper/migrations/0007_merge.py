f35a1a177342a49faf76b5953dc82dfeed826686
Florian Cech <loki@fsinf.at>
2017-05-26 12:28:08 +0200

    Merge migrations

---
+++ offenesparlament/op_scraper/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('op_scraper', '0006_auto_20170521_1018'),
+        ('op_scraper', '0006_remove_subscribedcontent_latest_content'),
+    ]
+
+    operations = [
+    ]
