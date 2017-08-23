392294f6fbe4dab644db7e482f5e668112819832
fin <fin+github@xbhd.org>
2017-08-24 00:21:05 +0200

    fix requirements; add merge migration

---
+++ offenesparlament/op_scraper/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('op_scraper', '0007_merge'),
+        ('op_scraper', '0011_commentedcontent_admin_notification_sent'),
+    ]
+
+    operations = [
+    ]
