65cac166a9b40e52a50bbe27770c6f43af66e052
fin <fin+github@xbhd.org>
2017-08-26 23:11:34 +0200

    enable all tests

--- offenesparlament/bin/test.sh
+++ offenesparlament/bin/test.sh
@@ -2,13 +2,13 @@
 
 ## Law and Laws
 # env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_law
-# env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_law.LawSubscriptionsTestCase.test_process_email_law_subscription
-# env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_law.LawsSubscriptionsTestCase.test_process_email_laws_subscription
+env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_law.LawSubscriptionsTestCase.test_process_email_law_subscription
+env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_law.LawsSubscriptionsTestCase.test_process_email_laws_subscription
 
 ## Person and Persons
 # env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_person
 env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_person.PersonSubscriptionsTestCase.test_process_email_person_subscription
-# env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_person.PersonsSubscriptionsTestCase.test_process_email_persons_subscription
+env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests.test_subscriptions_person.PersonsSubscriptionsTestCase.test_process_email_persons_subscription
 
 ## All tests
 # env DJANGO_CONFIGURATION=UnitTest python manage.py test op_scraper.tests
