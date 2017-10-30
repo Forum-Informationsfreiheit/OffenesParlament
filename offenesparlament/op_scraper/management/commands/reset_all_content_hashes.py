84d4174c23f9b4226098f5dc3e92ec0e0a665987
fin <fin+github@xbhd.org>
2017-10-30 08:41:11 +0100

    reset_all_content_hashes: fix haystack setup

--- offenesparlament/op_scraper/management/commands/reset_all_content_hashes.py
+++ offenesparlament/op_scraper/management/commands/reset_all_content_hashes.py
@@ -5,10 +5,7 @@ class Command(BaseCommand):
     help = 'Resets all subscribed content hashes'
 
     def handle(self, *args, **options):
-        from elasticsearch import Elasticsearch
-        es = Elasticsearch(retry_on_timeout=True)
-        if not es.indices.exists('archive'):
-            es.indices.create('archive')
+        haystack.connections['archive'].get_backend().setup()
         for sc in SubscribedContent.objects.all():
             sc.reset_content_hashes()
 
